083275a7ea0ea1fa59600275636bf648717749f0
JavierAcosta07 <a6521150022@utch.edu.mx>
2023-04-04 18:06:35 -0700

    Cambiado de color (Discusión) Insertado de cards

--- src/pages/components/Search.jsx
+++ src/pages/components/Search.jsx
@@ -1,7 +1,7 @@
 export default function Search(){
     return(
         <>
-            <input  type="text" placeholder="What do you want?" className="mt-10 w-80 input input-bordered input-warning  max-w-xs" />
+            <input  type="text" placeholder="What do you want?" className="mt-10 w-80 input input-bordered  max-w-xs" />
             {/* Imput de tipo texto para las busquedas, Aun falta meterlo dentro de un form en el index */}
         </>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import Bubble from './components/Bubble'
 import Search from './components/Search'
 
@@ -5,18 +8,62 @@ export default function Home() {
   return (
     <>
     {/* Maquetacion inicial */}
-    <div className="flex items-center justify-center "> {/* Div alinea al centro el contenido */}
-      <div class="flex-col w-100"> {/* Div alinea los elementos para que se coloquen encima de otros */}
-
-        <h1 className='mt-10 ml-[51px] text-warning text-5xl'>Best Price</h1> {/* Titulo y leyenda, Se les agrega un margen a  la izquierda {ml} para alinear */}
-        <p className=' mt-2 ml-[85px] text-warning'>The best price for all.</p>
-
-        <Search/> {/* Barra de busqueda, proviene de ./components/Search */}
-
-      </div>
-    </div>
-
     
+            <div className="flex items-center justify-center "> {/* Div alinea al centro el contenido */}
+              <div class="flex-col w-100"> {/* Div alinea los elementos para que se coloquen encima de otros */}
+
+                  <h1 className='mt-10 ml-[51px] text-5xl'>Best Price</h1> {/* Titulo y leyenda, Se les agrega un margen a  la izquierda {ml} para alinear */}
+                  <p className=' mt-2 ml-[85px]'>The best price for all.</p>
+
+                <Search/> {/* Barra de busqueda, proviene de ./components/Search */}
+
+              </div>
+            </div>
+               <br/>
+                  <div className='flex'>
+                               <div className='flex mx-12'>
+                                      <div className='container block-line flex'>
+                                          <div className="card w-96 glass text-center text-neutral-content">   
+                                            <figure><Image src={require('../styles/Image/sin-imagen.jpg')} alt="car!"/></figure>
+                                                  <div className="card-body">
+                                                      <h2 className="card-title">Life hack</h2>
+                                                      <p>How to park your car at your garage?</p>
+                                                      <div className="card-actions justify-end">
+                                                      <button className="btn btn-primary">Learn now!</button>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                       </div> 
+                                       <div className='flex mx-12'>
+                                        <div className='container block-line flex'>
+                                            <div className="card w-96 glass text-center text-neutral-content">   
+                                                <figure><Image src={require('../styles/Image/sin-imagen.jpg')} alt="car!"/></figure>
+                                                    <div className="card-body">
+                                                        <h2 className="card-title">Life hack</h2>
+                                                        <p>How to park your car at your garage?</p>
+                                                        <div className="card-actions justify-end">
+                                                            <button className="btn btn-primary">Learn now!</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className='flex mx-12'>
+                                            <div className='container block-line flex'>
+                                                <div className="card w-96 glass text-center text-neutral-content">   
+                                                    <figure><Image src={require('../styles/Image/sin-imagen.jpg')} alt="car!"/></figure>
+                                                        <div className="card-body">
+                                                            <h2 className="card-title">Life hack</h2>
+                                                            <p>How to park your car at your garage?</p>
+                                                            <div className="card-actions justify-end">
+                                                                <button className="btn btn-primary">Learn now!</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>                                   
+                                    </div>  
     <Bubble/> {/* Burbujas de animacion provienen de  './components/Bubble' */}
   </>        
   )
